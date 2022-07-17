13685ce841ec95fd2a49d2c1a52502eb40f0058d
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-07-16 21:19:36 -0600

    cambios

--- Render.py
+++ Render.py
@@ -1,57 +1,69 @@
 from utilidades import *
 
-class Render(object):
+#Variables globales del archivo.
+framebuffer = []
+ancho, alto = 0, 0 
 
-    def __init__(self, width, height):
-        
-        #Se inicializan los valores de la ventana.
-        self.Width = width #Ancho de la ventana.
-        self.Height = height #Alto de la ventana.
-        self.framebuffer = [] #Mapa de bits.
-        #print(self.framebuffer)
+#Método para poder escribir el framebuffer del archivo de imagen.
+def FrameBuffer(r, g, b):
+    global framebuffer #Variable global para modificar el framebuffer del archivo.
 
+    #Llenando de bits el framebuffer.
+    framebuffer = [
+        [color(r, g, b) for x in range(ancho)]
+        for y in range(alto)
+    ]
 
-        #Método que sirve para poder crear el archivo de imagen.
-        def write():
+def Render(Width, Height):
+    global ancho, alto #Variables globales del archivo que se modificarán.
 
-            #Se abre el archivo con la forma de bw.
-            f = open("SR1.bmp", "bw")
+    #Llenando las variables globaloes.
+    ancho = Width
+    alto = Height
 
-            #Haciendo el pixel header.
-            f.write(char('B'))
-            f.write(char('M'))
-            #Escribiendo el tamaño del archivo en bytes.
-            f.write(dword(14 + 40 + self.Width * self.Height * 3))
-            f.write(dword(0)) #Cosa que no se utilizará en este caso.
-            f.write(dword(14 + 40)) #Offset a la información de la imagen. 14 bytes para el header, 40 para la información de la imagen. Aquí empieza la data.
-            #Lo anterior suma 14 bytes.
+    #Método que sirve para poder crear el archivo de imagen.
+    def write():
 
-            #Información del header.
-            f.write(dword(40)) #Este es el tamaño del header. Esto es de 4 bytes, por eso se utiliza el dword.
-            f.write(dword(self.Width)) #Ancho de la imagen. Esto es de 4 bytes, por eso se utiliza el dword.
-            f.write(dword(self.Height)) #Alto de la imagen. Esto es de 4 bytes, por eso se utiliza el dword.
-            f.write(word(1)) #Número de planos. Esto es de 2 bytes, por eso se utiliza el word.
-            f.write(word(24)) #24 bits por pixel. Esto es porque usa el true color y el RGB.
-            f.write(dword(0)) #Esto es la compresión. Esto es de 4 bytes, por eso se utiliza el dword.
-            f.write(dword(self.Width * self.Height * 3)) #Tamaño de la imagen sin el header.
-            #Pixels que no se usarán mucho.
-            f.write(dword(0))
-            f.write(dword(0))
-            f.write(dword(0))
-            f.write(dword(0))
-            #Lo anterior suma 40 bytes.
+        #Se abre el archivo con la forma de bw.
+        f = open("SR1.bmp", "bw")
 
-            #Pintando el archivo de color negro.
-            #  for x in range(self.Height):
-            #      for y in range(self.Width):
-            #          f.write(self.framebuffer[x][y])
+        #Se escribe el encabezado del archivo.
 
-            f.close() #Cerrando el archivo que se escribió.
+        #Haciendo el pixel header.
+        f.write(char('B'))
+        f.write(char('M'))
+        #Escribiendo el tamaño del archivo en bytes.
+        f.write(dword(14 + 40 + Width * Height * 3))
+        f.write(dword(0)) #Cosa que no se utilizará en este caso.
+        f.write(dword(14 + 40)) #Offset a la información de la imagen. 14 bytes para el header, 40 para la información de la imagen. Aquí empieza la data.
+        #Lo anterior suma 14 bytes.
 
-        #Creando punto para debuggear la creación del archivo. Preguntar si está bien esta función en esta clase.
-        def point(x, y, color):
-            #Se escribe el pixel en la posición x, y con el color.
-            self.framebuffer[x][y] = color
+        #Información del header.
+        f.write(dword(40)) #Este es el tamaño del header. Esto es de 4 bytes, por eso se utiliza el dword.
+        f.write(dword(Width)) #Ancho de la imagen. Esto es de 4 bytes, por eso se utiliza el dword.
+        f.write(dword(Height)) #Alto de la imagen. Esto es de 4 bytes, por eso se utiliza el dword.
+        f.write(word(1)) #Número de planos. Esto es de 2 bytes, por eso se utiliza el word.
+        f.write(word(24)) #24 bits por pixel. Esto es porque usa el true color y el RGB.
+        f.write(dword(0)) #Esto es la compresión. Esto es de 4 bytes, por eso se utiliza el dword.
+        f.write(dword(Width * Height * 3)) #Tamaño de la imagen sin el header.
+        #Pixels que no se usarán mucho.
+        f.write(dword(0))
+        f.write(dword(0))
+        f.write(dword(0))
+        f.write(dword(0))
+        #Lo anterior suma 40 bytes.
 
-        #point(200, 400, color) #Creando el punto.
-        #write() #Llamando al método que escribirá el archivo.
+        # #Pintando el archivo de color negro.
+        # for x in range(Height):
+        #     for y in range(Width):
+        #         f.write(framebuffer[x][y])
+
+        f.close() #Cerrando el archivo que se escribió.
+
+    #Creando punto para debuggear la creación del archivo. Preguntar si está bien esta función en esta clase.
+    def point(x, y, color):
+        #Se escribe el pixel en la posición x, y con el color.
+        framebuffer[x][y] = color
+
+    #point(200, 400, color) #Creando el punto.
+    write() #Llamando al método que escribirá el archivo.
--- gl.py
+++ gl.py
@@ -42,12 +42,11 @@ def glCreateWindow(width, height): #Preguntar de esta función.
         #Saber si las dimensiones son múltiplos de 4.
         if width % 4 == 0 and height % 4 == 0:
             
-            #Llenando variables globales.
-
+            #Llenando variables globales. Estas son para el ancho de la ventana y el alto de la ventana.
             anchoV = width 
             altoV = height
 
-            Rend.Render(width, height) #Creando la ventana.
+            Rend.Render(anchoV, altoV) #Creando la ventana.
 
         elif width < 0 or height < 0: #Si las dimensiones son negativas, entonces se imprime un error.
             print("Error")
@@ -80,7 +79,6 @@ def glViewPort(x, y, width, height): #Se usará para definir el área de la imag
 
 #Preguntar si esta función lo que hace es llenar por primera vez el color de la pantalla.
 def glClear(): #Se usará para que llene el mapa de bits con un solo color.
-    global Rend #Se usa para poder acceder a la variable global render.
     
 
     if rP < 0 or gP < 0 or bP < 0: #Si los colores son menores a 0, entonces se imprime un error.
@@ -88,12 +86,7 @@ def glClear(): #Se usará para que llene el mapa de bits con un solo color.
     elif rP > 1 or gP > 1 or bP > 1:
         print("Error")
     else: #Si todo está bien, entonces se llena el mapa de bits con el color que se le pasa.
-    
-    #Enviar el mapa de bits a la clase Render.
-      Rend.Render.framebuffer = [
-            [glColor(rP, gP, bP) for x in range(anchoV)]
-            for y in range(altoV)
-        ]
+        Rend.FrameBuffer(rP, gP, bP) #Llenando el mapa de bits con un color.
 
     #Debugging.
     #print(anchoV)
@@ -117,6 +110,10 @@ def glClearColor(r, g, b): #Función con la que se pueda cambiar el color con el
         rP = r
         gP = g
         bP = b
+        
+        color(rP, gP, bP) #Color inicial de la pantalla.
+
+        print(color(rP, gP, bP)) #Debuggeo.
 
 def glVertex(x, y): #Función que pueda cambiar el color de un punto de la pantalla. Las coordenadas x, y son relativas al viewport que definieron con glViewPort. glVertex(0, 0) cambia el color del punto en el centro del viewport, glVertex(1, 1) en la esquina superior derecha. glVertex(-1, -1) la esquina inferior izquierda
     return ""
@@ -135,7 +132,7 @@ def glFinish(): #Función que escribe el archivo de imagen resultante.
     global Rend
 
     #Llamar al método write en la clase Render.
-    Rend.Render.write()
+   # Rend.write()
 
 #print(glColor(1,1,1))
 
